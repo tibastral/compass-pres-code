547c749b622a85e462b9a044cd56d4ef8b1cb836
Thibaut Assus <thibaut.assus@gmail.com>
2011-02-07 18:14:37 -0500

    fisrt iteration, working copy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SassDemo::Application.config.secret_token = '1b7592f2f5082b0404b26c95002a7f5a0469fc430f70dad2a7ea50f27cc63989341117401217576673faf9a2ebd12828cb0eb9e24833b3a6ad34f550d967cdc1'
